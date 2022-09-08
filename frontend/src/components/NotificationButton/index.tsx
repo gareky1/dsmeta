73f31b445e921eaf34c9fc56589bf13f81caef51
Henrique Lira Da Silva <henrique.2019210942@unicap.br>
2022-09-07 22:15:38 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,15 @@
+import icone from '../../assets/img/Vector (2).svg'
+
+import './style.css'
+
+function NotificationButton() {
+    return (
+        <>
+        <div className="dsmeta-red-btn">
+            <img src={icone} alt="Notificar" />
+        </div>
+        </>
+    )
+}
+
+export default NotificationButton
